46023775f48788eda5f21e86736ebc8042292cb5
Uttuoo7 <94781094+Uttuoo7@users.noreply.github.com>
2026-02-25 04:02:56 +0530

    add secret key

--- backend/auth.py
+++ backend/auth.py
@@ -9,7 +9,7 @@ from passlib.context import CryptContext
 
 from backend.database import db
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY",  "dev-temporary-secret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
